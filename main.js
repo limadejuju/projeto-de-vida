48111f8b5570e86c94a03f62c8efa152d0ed9ad7
limadejuju <lima.juliano30@escola.pr.gov.br>
2026-05-08 09:41:46 -0300

    111

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ for (let i = 0; i < botoes.length; i++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
@@ -23,27 +22,35 @@ const tempoObjetivo4 = new Date("2032-07-25T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-}
-
-atualizaCronometro();
-setInterval(atualizaCronometro, 1000);
-
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-
+    
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
+    
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+     return "prazo Finalizado";
+    }
+    
 }
+
+function atualizaCronometro() {
+    for (let i = 0; i < consetInterval(atualizaCronometro, 1000);tadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
